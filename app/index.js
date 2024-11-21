dee0acf6a2eeaa060ceb3fb513ab6209df2ea83e
SidhGupta <158204477+SidhGupta@users.noreply.github.com>
2024-11-21 12:34:00 -0500

    b

--- app/index.js
+++ app/index.js
@@ -1,6 +1,9 @@
-const url = "https://api.pokemontcg.io/v2/cards"
 import "./style.css"
-async function getData(){
+const url = "https://api.pokemontcg.io/v2/cards"
+
+
+
+async function getData(url){
     //fetch returns a promise 
     try {
         const response = await fetch(url);
@@ -12,13 +15,34 @@ async function getData(){
         const data = await response.json();
         console.log(data);
         document.querySelector("h1").textContent = data.data;
+        displayCards(data);
     }
 }   catch (error) {
-
         console.log(error);
         alert("sorry could not find that Poket Monster")
     }
 }
 
+
+
+function createCards(data) {
+    
+    // Loop through each card in the data and create an HTML card for each
+    cards.forEach(card => {
+        document.querySelector(".container").insertAdjacentHTML('beforeend', `
+            <div class="card">
+                <h2>${card.name}</h2>
+                <img src="${card.images.small}" alt="${card.name}">
+                <h3>Legality: ${card.legalities}</h3>
+                <h3>Evolves to: ${card.evolvesTo ? card.evolvesTo : "N/A"}</h3>
+            </div>
+        `);
+    });
+}
+
+function displayCards(data) {
+    const container = document.getElementById("container")
+    container.innerHTML = createCards(data);
+}
+
 getData(url);
-    // gaurd clause);
